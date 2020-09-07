85278157abf6c467ffb3929e4014e1ace5d4f859
Twentysix <Twentysix26@users.noreply.github.com>
2020-09-07 04:16:20 +0200

    Fix compressed file with no extension

--- indexer.py
+++ indexer.py
@@ -11,7 +11,7 @@ RX_PROTOCOL = 1 # This should be incremented when breaking changes to the format
 GEN_PATH = Path("index")
 GEN_FILE = GEN_PATH / Path(f"{RX_PROTOCOL}.json") # Pretty, for QA checking
 GEN_MIN_FILE = GEN_PATH / Path(f"{RX_PROTOCOL}-min.json") # Minified, for user download
-GEN_GZ_FILE = GEN_PATH / Path(f"{RX_PROTOCOL}-min.gz") # Gzipped
+GEN_GZ_FILE = GEN_PATH / Path(f"{RX_PROTOCOL}-min.json.gz") # Gzipped
 GEN_ERROR_LOG = GEN_PATH / Path(f"{RX_PROTOCOL}-errors.yaml") # Error log
 
 
